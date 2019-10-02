2f07c66ecb95d799ff10b383fb961bdbcdcb5baf
Pablo Hauser <pabhau@az.zli.ch>
2019-10-02 09:15:11 +0200

    Classe User und Reservierung erstellt + alle Variablen Signed-off-by: Pablo Hauser <pabhau@az.zli.ch>

--- resver_rest/src/main/java/model/Restaurant.java
+++ resver_rest/src/main/java/model/Restaurant.java
@@ -1,5 +1,39 @@
 package model;
 
-public class Restaurant {
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Restaurant {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int rolle_id;	
+	
+	private int anzahlpersonen;
+	
+	
+	// Die Variablen date und time sind Strings und nicht wie in der Doku,
+	// weil es sonst zu umständlich wäre den Erhaltenen Datentyp von der Webseite zu konvertieren
+	private String date; 
+	private String time;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private int user_id_fk;
+	
+	/* Habe dies Auskommentiert da ich diese Tabelle nicht erstellen werde, da nicht gefordert 
+	@ManyToOne
+	@JoinColumn(name = "restaurant_id")
+	private int restaurant_id_fk;
+	*/
+	
+	
+	
 }
--- resver_rest/src/main/java/model/User.java
+++ resver_rest/src/main/java/model/User.java
@@ -5,25 +5,32 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
 @Entity
 public class User {
-
+	
+	//@ID Gibt den Primärschlüssel einer Entität 
+	//@GeneratedValue Gibt die Spezifikation von Generierungsstrategien für die Werte von Primärschlüsseln an. 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int user_id;
 	
-	@OneToOne
+	//@ManytoOne zeigt auf in welcher Beziehung die Entitäten zu einander stehen 
+	//Hier Mehrere zu eins, dass heisst das ein User nur Eine rolle haben kann aber mehrere User können die Gleiche rolle haben
+	//Hier ist mir in der Planung ein Denkfehler unterlaufen ( In der Doku 1zu1) zum Glück habe ich es hier bemerkt
+	@ManyToOne
 	@JoinColumn(name = "rolle_id")
 	private int rolle_id_fk;
 	
+	// String für Nachname, Vorname usw. 
 	private String nachname;
 	private String vorname;
 	private String login_name;
 	private String passwort;
 	
-	
+	//Getter und Setter für alle Variablen 
 	public int getUser_id() {
 		return user_id;
 	}
